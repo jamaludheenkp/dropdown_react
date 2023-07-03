c7c3d2f5545065bf8df7fbd54fc8a27a58dc9cc1
jamaludheen kp <jamaljazz1998@gmail.com>
2023-07-03 21:00:18 +0530

    clear all and start again

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import demoApi from './api/demoApi';
-import asyncSelect from 'react-select/async';
-
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+return (
+   <h1>hello world</h1>
+);
 }
 
 export default App;
